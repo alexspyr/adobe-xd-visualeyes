4e73204ee30956e28df7f252b477fec3536d05a3
draptis <dimitrisraptis96@gmail.com>
2019-08-05 17:33:33 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,158 @@
+const application = require("application");
+const { ImageFill, Rectangle } = require("scenegraph");
+const fs = require("uxp").storage.localFileSystem;
+const formats = require("uxp").storage.formats;
+const base64 = require("./base64-arraybuffer");
+
+async function generateHeatmap(selection) {
+  if (!selection.hasArtboards)
+    return createToastMessage(`👎 Please select an Artboard`);
+
+  const artboard = selection.items[0];
+
+  createToastMessage(`🧠 Your heatmap is generating...`);
+
+  // Export Artboard to temporary image file
+  const folder = await fs.getTemporaryFolder();
+  const file = await folder.createFile("tmp", { overwrite: true });
+  const renditionSettings = [
+    {
+      node: artboard,
+      outputFile: file,
+      type: application.RenditionType.JPG,
+      scale: 1,
+      quality: 100,
+    },
+  ];
+
+  const rectangle = new Rectangle();
+  createHeatmapLayer(rectangle, artboard);
+
+  const imageFill = await application
+    .createRenditions(renditionSettings)
+    .then(async (results) => {
+      const binary = await results[0].outputFile.read({
+        format: formats.binary,
+      });
+      const imgBase64 = "data:image/jpg;base64," + base64.encode(binary);
+
+      const url = await uploadImage(imgBase64);
+      const fill = await getImageFill(rectangle, url);
+
+      return fill;
+    });
+
+  rectangle.fill = imageFill;
+  createToastMessage(`🎉 Bazinga!`);
+}
+
+function createHeatmapLayer(rectangle, artboard) {
+  rectangle.name = "VisualEyes Heatmap";
+  rectangle.width = artboard.width;
+  rectangle.height = artboard.height;
+  artboard.addChild(rectangle);
+  rectangle.moveInParentCoordinates(0, 0);
+}
+
+function uploadImage(image) {
+  const formData = new FormData();
+  formData.append("isTransparent", "false");
+  formData.append("image", image);
+
+  return new Promise(function(resolve) {
+    fetch("https://api.visualeyes.loceye.io/predict/", {
+      method: "POST",
+      body: formData,
+      headers: {
+        Authorization: `Token loceye-user-unlimited`,
+        // "Content-Type": "application/x-www-form-urlencoded",
+        "cache-control": "no-cache",
+      },
+    })
+      .then((res) => {
+        const { status } = res;
+
+        if (status === 200) {
+          console.log("Successful");
+        } else if (status === 403) {
+          createToastMessage(`🚨 Your heatmaps limit has been exceeded`);
+        } else {
+          createToastMessage(
+            `😱 We are deeply sorry, but something went terrible wrong!`
+          );
+        }
+        return res.json();
+      })
+      .then((json) => {
+        if (json.code !== "success") {
+          throw new Error("Error during fetching the heatmap");
+        }
+        const heatmapURL = json.url;
+        resolve(heatmapURL);
+        // createToastMessage(`🎉 Bazinga!`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+}
+
+function xhrBinary(url) {
+  return new Promise((resolve, reject) => {
+    const req = new XMLHttpRequest();
+    req.onload = () => {
+      if (req.status === 200) {
+        try {
+          const arr = new Uint8Array(req.response);
+          resolve(arr);
+        } catch (err) {
+          reject(`Couldnt parse response. ${err.message}, ${req.response}`);
+        }
+      } else {
+        reject(`Request had an error: ${req.status}`);
+      }
+    };
+    req.onerror = reject;
+    req.onabort = reject;
+    req.open("GET", url, true);
+    req.responseType = "arraybuffer";
+    req.send();
+  });
+}
+
+async function getImageFill(rectangle, url) {
+  try {
+    const photoObj = await xhrBinary(url);
+    const tempFolder = await fs.getTemporaryFolder();
+    const file = await tempFolder.createFile("tmp", { overwrite: true });
+    await file.write(photoObj, { format: formats.binary });
+    const imageFill = new ImageFill(file);
+    return imageFill;
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+function createToastMessage(message) {
+  document.body.innerHTML = `
+    <dialog id="dialog">
+      <form>
+        <p>${message}</p>
+        <footer>
+          <button id="button" type="submit" uxp-variant="primary">Close</button>
+        </footer>
+      </form>
+    </dialog>
+  `;
+  const dialog = document.querySelector("#dialog");
+  dialog.addEventListener("submit", () => dialog.remove());
+
+  setTimeout(() => dialog.remove(), 5000);
+  dialog.showModal();
+}
+
+module.exports = {
+  commands: {
+    generateHeatmap,
+  },
+};
